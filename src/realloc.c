6c655ca051c542a06380f8c276fa60244aefc31f
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-02-07 19:28:52 +0100

    Refactor realloc

--- src/realloc.c
+++ src/realloc.c
@@ -6,17 +6,11 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/09 18:17:26 by lde-moul          #+#    #+#             */
-/*   Updated: 2022/02/07 19:00:21 by lde-moul         ###   ########.fr       */
+/*   Updated: 2022/02/07 19:27:55 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
-
-static void	*unlock_and_return(void *ptr)
-{
-	pthread_mutex_unlock(&g_mutex);
-	return (ptr);
-}
 
 static size_t	space_after_block(t_zone *zone, t_block *block)
 {
@@ -44,28 +38,35 @@ static void	move_content_to_new_location(void *old_ptr, void *new_ptr,
 	}
 }
 
-void	*realloc(void *ptr, size_t size)
+static void	*base_realloc(void *ptr, size_t size)
 {
 	t_zone	**ptr_zone;
 	t_block	**ptr_block;
 	void	*new_ptr;
 
-	pthread_mutex_lock(&g_mutex);
 	if (!ptr)
-		return (unlock_and_return(base_malloc(size)));
+		return (base_malloc(size));
 	find_ptr(ptr, &ptr_zone, &ptr_block);
 	if (!ptr_block)
-		return (unlock_and_return(NULL));
+		return (NULL);
 	if ((*ptr_block)->size + space_after_block(*ptr_zone, *ptr_block)
 		>= size && size)
 	{
 		(*ptr_block)->size = size;
-		return (unlock_and_return(ptr));
+		return (ptr);
 	}
 	new_ptr = base_malloc(size);
 	if (!new_ptr)
-		return (unlock_and_return(NULL));
+		return (NULL);
 	move_content_to_new_location(ptr, new_ptr, (*ptr_block)->size, size);
 	remove_block(ptr_zone, ptr_block);
-	return (unlock_and_return(new_ptr));
+	return (new_ptr);
+}
+
+void	*realloc(void *ptr, size_t size)
+{
+	pthread_mutex_lock(&g_mutex);
+	ptr = base_realloc(ptr, size);
+	pthread_mutex_unlock(&g_mutex);
+	return (ptr);
 }
