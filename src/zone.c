827f3799e888a9d1c680f8d62bb5fadecdc5c952
Louis-Antoine DE MOULINS DE ROCHEFORT <lde-moul@student.42.fr>
2022-02-08 20:25:09 +0100

    Allocate extra size for LARGE zones

--- src/zone.c
+++ src/zone.c
@@ -6,12 +6,26 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 18:16:39 by lde-moul          #+#    #+#             */
-/*   Updated: 2020/02/06 18:18:22 by lde-moul         ###   ########.fr       */
+/*   Updated: 2022/02/08 20:05:39 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 #include <unistd.h>
+
+static size_t	get_rounded_size(size_t size)
+{
+	size += sizeof(t_zone);
+	return ((size_t)align_up((void *)size, getpagesize()));
+}
+
+static size_t	get_wasted_size(size_t block_size)
+{
+	size_t	zone_size;
+
+	zone_size = get_rounded_size(block_size) - sizeof(t_zone);
+	return (zone_size % block_size);
+}
 
 uintptr_t	zone_end(t_zone *zone)
 {
@@ -34,7 +48,10 @@ void	get_zone_size_and_type(size_t block_size,
 	else if (*zone_type == 1)
 		*zone_size = 100 * (SMALL + sizeof(t_block));
 	else
+	{
 		*zone_size = block_size + sizeof(t_block);
-	*zone_size += sizeof(t_zone);
-	*zone_size = (size_t)align_up(((void *)*zone_size), getpagesize());
+		if (16 * get_wasted_size(*zone_size) > get_rounded_size(*zone_size))
+			*zone_size *= 16;
+	}
+	*zone_size = get_rounded_size(*zone_size);
 }
